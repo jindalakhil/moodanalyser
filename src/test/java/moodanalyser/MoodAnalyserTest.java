06ad4e0ebcf7f898d2dbd34565365c066e348d52
jindalakhil <akhiljindal222@gmail.com>
2020-10-01 03:49:43 +0530

    [Akhil]Added: Handled Exception Test Case for null

--- src/test/java/moodanalyser/MoodAnalyserTest.java
+++ src/test/java/moodanalyser/MoodAnalyserTest.java
@@ -18,4 +18,11 @@ public class MoodAnalyserTest {
 		Assert.assertEquals("HAPPY", moodfinder.analyseMood());
 	}
 	
+	@Test(expected = NullPointerException.class)
+	public void givenMsg_When_Null() {
+		MoodAnalyser moodfinder = new MoodAnalyser(null);
+		//String mood = moodfinder.analyseMood("this is happy message");
+		 moodfinder.analyseMood();
+	}
+	
 }
